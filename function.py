b44b6f18131f734a89b4a7db2f547dd91276602a
Maisha Thasin <thasin.maisha@gmail.com>
2024-03-30 17:20:35 -0400

    add the upload storage

--- function.py
+++ function.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import uuid
 import os
+from google.cloud import storage
 
 def initialize_driver():
     driver = webdriver.Chrome() 
@@ -19,9 +20,9 @@ def screenshot_with_highlight(URL):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     # TODO: The demo person need to update the binary_location and user-data-dir
-    options.binary_location = r"C:/Users/vince/AppData/Local/Google/Chrome SxS/Application/chrome.exe"
+    #options.binary_location = r"C:/Users/vince/AppData/Local/Google/Chrome SxS/Application/chrome.exe"
     # Maybe specify the profile directory "/Profile 1"
-    options.add_argument(r"user-data-dir=C:/Users/vince/AppData/Local/Google/Chrome SxS/User Data")
+    #options.add_argument(r"user-data-dir=C:/Users/vince/AppData/Local/Google/Chrome SxS/User Data")
     options.add_argument("--profile-directory=Default");
 
     driver = webdriver.Chrome(options=options)
@@ -38,7 +39,44 @@ def screenshot_with_highlight(URL):
 
     os.makedirs("screenshots", exist_ok=True)
     filename = f"screenshots/screenshot_{str(uuid.uuid4())}.png"
+
     driver.save_screenshot(filename)
+
+    upload_to_storage(filename)
+    
+
+    ## ToDO: upload each of these screenshots to Google cloud storage 
+
+# this is gor uplpoading to storage 
+def upload_to_storage(filename):
+    client = storage.Client()
+    bucket_name = "genai-genesis"
+    bucket = client.bucket(bucket_name)
+
+    blob_name = os.path.basename(filename)
+    blob = bucket.blob(blob_name)
+
+    with open(filename, "rb") as file:
+        blob.upload_from_file(file)
+
+    print(f"Screenshot uploaded to Google Cloud Storage: {blob_name}")
+
+
+# test this , 
+def get_latest_screenshot():
+    client = storage.Client()
+    bucket_name = "genai-genesis"
+    bucket = client.bucket(bucket_name)
+
+    blobs = list(bucket.list_blobs(prefix="screenshot_"))
+    latest_blob = max(blobs, key=lambda blob: blob.time_created)
+
+    filename = f"screenshots/{latest_blob.name}"
+    latest_blob.download_to_filename(filename)
+
+    return filename
+
+
 
 def click_element(driver, xpath):
     element = WebDriverWait(driver, 10).until(
