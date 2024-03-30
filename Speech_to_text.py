0d1309526ae0046cb99e4a7543543390801576fc
nmdangg <nm.dang@mail.utoronto.ca>
2024-03-30 16:47:02 -0400

    added api

--- Speech_to_text.py
+++ Speech_to_text.py
@@ -1,7 +1,7 @@
 
 # code from:
 # https://cloud.google.com/speech-to-text/docs/transcribe-streaming-audio?_gl=1*3m4ohq*_up*MQ..&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA_F8euqgZnju9joV5k9OUT9cKogYZd-aQzcKdujGSYm8Vz4eSju0bUaAmFUEALw_wcB&gclsrc=aw.ds#perform_streaming_speech_recognition_on_an_audio_stream
-
+import os
 import queue
 import re
 import sys
@@ -114,67 +114,97 @@ class MicrophoneStream:
             yield b"".join(data)
 
 
+# def listen_print_loop(responses: object) -> str:
+#     """Iterates through server responses and prints them.
+#
+#     The responses passed is a generator that will block until a response
+#     is provided by the server.
+#
+#     Each response may contain multiple results, and each result may contain
+#     multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+#     print only the transcription for the top alternative of the top result.
+#
+#     In this case, responses are provided for interim results as well. If the
+#     response is an interim one, print a line feed at the end of it, to allow
+#     the next result to overwrite it, until the response is a final one. For the
+#     final one, print a newline to preserve the finalized transcription.
+#
+#     Args:
+#         responses: List of server responses
+#
+#     Returns:
+#         The transcribed text.
+#     """
+#     num_chars_printed = 0
+#     for response in responses:
+#         if not response.results:
+#             continue
+#
+#         # The `results` list is consecutive. For streaming, we only care about
+#         # the first result being considered, since once it's `is_final`, it
+#         # moves on to considering the next utterance.
+#         result = response.results[0]
+#         if not result.alternatives:
+#             continue
+#
+#         # Display the transcription of the top alternative.
+#         transcript = result.alternatives[0].transcript
+#
+#         # Display interim results, but with a carriage return at the end of the
+#         # line, so subsequent lines will overwrite them.
+#         #
+#         # If the previous result was longer than this one, we need to print
+#         # some extra spaces to overwrite the previous result
+#         overwrite_chars = " " * (num_chars_printed - len(transcript))
+#
+#         if not result.is_final:
+#             sys.stdout.write(transcript + overwrite_chars + "\r")
+#             sys.stdout.flush()
+#
+#             num_chars_printed = len(transcript)
+#
+#         else:
+#             print(transcript + overwrite_chars)
+#
+#             # Exit recognition if any of the transcribed phrases could be
+#             # one of our keywords.
+#             if re.search(r"\b(exit|quit)\b", transcript, re.I):
+#                 print("Exiting..")
+#                 break
+#
+#             num_chars_printed = 0
+#
+#     return transcript
+
 def listen_print_loop(responses: object) -> str:
-    """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
+    """Iterates through server responses and concatenates them into a single string.
 
     Args:
         responses: List of server responses
 
     Returns:
-        The transcribed text.
+        The concatenated transcribed text.
     """
-    num_chars_printed = 0
+    full_transcription = ""  # Initialize an empty string to hold the transcriptions
     for response in responses:
         if not response.results:
             continue
 
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
         result = response.results[0]
         if not result.alternatives:
             continue
 
-        # Display the transcription of the top alternative.
         transcript = result.alternatives[0].transcript
 
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = " " * (num_chars_printed - len(transcript))
+        if result.is_final:
+            full_transcription += transcript + " "  # Add the transcription to the string
 
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + "\r")
-            sys.stdout.flush()
-
-            num_chars_printed = len(transcript)
-
-        else:
-            print(transcript + overwrite_chars)
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
+            # Optionally, you can also check for exit commands within this loop
             if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                print("Exiting..")
-                break
+                break  # If you find an exit command, break from the loop
 
-            num_chars_printed = 0
+    return full_transcription.strip()  # Return the full transcription, stripped of leading/trailing whitespace
 
-    return transcript
 
 
 def main() -> None:
@@ -182,6 +212,10 @@ def main() -> None:
     # See http://g.co/cloud/speech/docs/languages
     # for a list of supported languages.
     language_code = "en-US"  # a BCP-47 language tag
+
+    # Set the path to the service account key file, can change it
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = \
+        "/Users/minhdang/Documents/school/4th year/genai_hack/web-agent/key.json"
 
     client = speech.SpeechClient()
     config = speech.RecognitionConfig(
@@ -204,7 +238,10 @@ def main() -> None:
         responses = client.streaming_recognize(streaming_config, requests)
 
         # Now, put the transcription responses to use.
-        listen_print_loop(responses)
+        # listen_print_loop(responses)
+        transcribed_text = listen_print_loop(responses)
+        print("Full Transcription:", transcribed_text)
+
 
 
 if __name__ == "__main__":
