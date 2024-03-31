ee51db58e4e35b257e9375ef1d77675cedb3dccb
nmdangg <nm.dang@mail.utoronto.ca>
2024-03-30 20:41:32 -0400

    text to speech

---
+++ text_to_speech.py
@@ -0,0 +1,28 @@
+import os
+from google.cloud import texttospeech
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
+
